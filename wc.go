6d5e1cd5d4d4b9cf267f0c37a57b60dbdcfff29b
e.blagodarev <ze0nni@gmail.com>
2020-04-20 17:52:14 +0300

    Calc ascii symbols for each files

--- wc.go
+++ wc.go
@@ -1,17 +1,77 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/karrick/godirwalk"
 )
 
+const asciiMapSize = 127
+
+type asciiMap = [asciiMapSize]int64
+
 func main() {
-	for f := range scanDir(".") {
-		fmt.Printf("%s\n", f)
+	run(4, scanDir("."))
+}
+
+func run(numGorutines int, files <-chan string) {
+	var wg = sync.WaitGroup{}
+
+	for i := 0; i < numGorutines; i++ {
+		wg.Add(1)
+
+		go func() {
+			task(files)
+			defer wg.Done()
+		}()
 	}
+	wg.Wait()
+}
+
+func task(files <-chan string) {
+	for f := range files {
+		_, err := scanFile(f)
+		if nil != err {
+			log.Panic(err)
+		}
+	}
+}
+
+// Тут asciiMap передается через стек, что, думаю, совсем не плохо
+func scanFile(fileName string) (asciiMap, error) {
+	var asciiMap asciiMap
+
+	file, err := os.Open(fileName)
+	if nil != err {
+		return asciiMap, err
+	}
+
+	defer file.Close()
+
+	buffer := make([]byte, 1024*60)
+
+ReadLoop:
+	for {
+		readed, err := file.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				return asciiMap, err
+			}
+			break ReadLoop
+		}
+
+		for i := 0; i < readed; i++ {
+			b := buffer[i]
+			if b < 127 {
+				asciiMap[b]++
+			}
+		}
+	}
+
+	return asciiMap, nil
 }
 
 func scanDir(root string) <-chan string {
@@ -42,4 +102,24 @@ func scanDir(root string) <-chan string {
 	}()
 
 	return out
+}
+
+func run(numGorutines int, files <-chan string) {
+	var wg = sync.WaitGroup{}
+
+	for i := 0; i < numGorutines; i++ {
+		wg.Add(1)
+
+		go func() {
+			task(files)
+			defer wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func task(files <-chan string) {
+	for range files {
+
+	}
 }
